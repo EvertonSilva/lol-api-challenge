864af04e9cac1f8917f6347224ee58c7cad57b55
Everton Silva <thonns@gmail.com>
2016-10-13 02:30:11 -0300

    Listing champions

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,15 @@
-require 'haml'
+require 'json'
+require_relative './data_loader'
+require_relative './champion'
 
 get '/' do
-	@name = "Home"
+	loader = DataLoader.new
+	loader.set_path
+
+	resp = loader.make_request("locale"=>"pt_BR", "champData"=>"image,recommended")
+	data_hash = JSON.parse(resp.body) if resp.code == "200"
+
+	@champs = data_hash['data']
+
 	haml :index
 end
